9717215d9c67639d8172e2984e406d02579ff502
himoura32 <shawn.ahearn@outlook.com>
2022-05-30 15:53:51 -0400

    coded dropdown comp/useRef/state/manualEvent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import Accordian from './components/Accordian';
+import { useState } from 'react';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Accordian from './components/Accordian';
 import './components/App.css';
 
 const items = [
@@ -17,10 +19,38 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red',
+  },
+  {
+    label: 'The color Green',
+    value: 'green',
+  },
+  {
+    label: 'The Shade of Blue',
+    value: 'blue',
+  },
+];
+
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div className='ui container'>
-      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
+      {/* <Search /> */}
       {/* <Accordian items={items} /> */}
     </div>
   );
